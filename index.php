91efdef89303d17e3589f574f71448cff7ed287a
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-23 22:19:48 +0600

    datainsert

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+<body>
+    <?php
+    ?>
+<form method="POST">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6" offset-md-3>
+                <div class="form-group">
+                    <label for="studentName">Student Name</label>
+                    <input type="text" class="form-control" name="studentName">
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
