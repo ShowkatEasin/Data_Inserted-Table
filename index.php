d5acba47da7f52ef20a59dea1123979c816b7845
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-23 23:56:56 +0600

    Inserted

--- index.php
+++ index.php
@@ -6,21 +6,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 <body>
+
+
     <?php
+     $hostName = "localhost";
+     $connection = new mysqli ("localhost","root","","batch05_php");
+
+     if(isset($_POST["send"])){
+
+        
+        $studentName = $_POST['studentName'];
+        $fName = $_POST['fName'];
+        $mName = $_POST['mName'];
+        $email = $_POST['email'];
+        $status = $_POST['status'];
+
+       /*  $insert = "INSERT INTO tbl_student(studentName,fName,mName,email,status)
+        VALUES('$studentName','$fName','$mName','$email','$status')";
+ */
+       $statements = "INSERT INTO tbl_student(studentName,fName,mName,email,status)VALUES('$studentName','$fName','$mName','$email','$status')";
+
+       $result = $connection->query($statements);
+       
+
+       if ($result){
+        echo "Data Inserted";
+       }
+       else {
+        echo "Error";
+       }
+    }
+
+
     ?>
-<form method="POST">
+
     <div class="container">
-        <div class="row">
+        <div class="row mt-5">
             <div class="col-md-6" offset-md-3>
-                <div class="form-group">
+
+            <form method="POST">
+                <div class="form-group ">
                     <label for="studentName">Student Name</label>
-                    <input type="text" class="form-control" name="studentName">
+                    <input id="studentName" type="text" class="form-control" name="studentName">
                 </div>
+                <div class="form-group mt-3">
+                    <label for="fName">Father's Name</label>
+                    <input id="fName" type="text" class="form-control" name="fName">
+                </div>
+                <div class="form-group  mt-3">
+                    <label for="mName">Mother's Name</label>
+                    <input id="mName" type="text" class="form-control" name="mName">
+                </div>
+                <div class="form-group  mt-3">
+                    <label for="email">Email Address</label>
+                    <input id="email" type="text" class="form-control" name="email">
+                </div>
+
+                <div class="form-group  mt-3">
+                    <label for="status">Status</label>
+                    <select name="status" id="status" class="form-control">
+                        <option value="">--Select Status--</option>
+                        <option value="1">Active</option>
+                        <option value="2">Inactive</option>
+                    </select>
+                </div>
+                <button name="send" class="btn btn-success mt-3">Send</button>
+
+                </form>
             </div>
         </div>
     </div>
-    </form>
+   
 </body>
 </html>
