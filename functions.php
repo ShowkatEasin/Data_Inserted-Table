35e9bc14e5b6f98494c1eac16073a14db388e3c4
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-25 19:53:56 +0600

    Done

--- functions.php
+++ functions.php
@@ -1,130 +1,113 @@
-<?php
+<?php 
 
-function insert($fromData){
-    $connection = new mysqli ("localhost","root","","batch05_php");
-    
-    $studentName = $fromData["studentName"];
-    $fName = $fromData['fName'];
-    $mName = $fromData['mName'];
-    $email = $fromData['email'];
-    $status= $fromData['status'];
-    
-   if($studentName==""){
-    echo '<div class="alert alert-danger"><strong>Name Field is Empty</strong></div>';
-   }
-   elseif ($fName==""){
-    echo '<div class="alert alert-danger"><strong>Fathers Name is Empty</strong></div>';
-   }
-   elseif ($mName==""){
-    echo '<div class="alert alert-danger"><strong>Mothers Name is Empty</strong></div>';
-   }
-   elseif ($email==""){
-    echo '<div class="alert alert-danger"><strong>Email field is Empty</strong></div>';
-   }
-   elseif ($status==""){
-    echo '<div class="alert alert-danger"><strong>Select Status</strong></div>';
-   }
+	$connection = new mysqli("localhost","root","","batch05_php");
+		
+	function insert($fromData){
+		global $connection;
+		$studentName = $fromData['studentName'];
+		$fName = $fromData['fName'];
+		$mName = $fromData['mName'];
+		$email = $fromData['email'];
+		$status = $fromData['status'];
+		if($studentName==""){
+			echo '<div class="alert alert-danger"><strong>Error : </strong>Name Field is Empty</div>';
+		}
+		elseif($fName==""){
+			echo '<div class="alert alert-danger"><strong>Error : </strong>Fathers Name Field is Empty</div>';
+		}
+		elseif($mName==""){
+			echo '<div class="alert alert-danger"><strong>Error : </strong>Mothers Name Field is Empty</div>';
+		}
+		elseif($email==""){
+			echo '<div class="alert alert-danger"><strong>Error : </strong>Email Field is Empty</div>';
+		}
+		elseif($status==""){
+			echo '<div class="alert alert-danger"><strong>Error : </strong>Status Field is Empty</div>';
+		}
+		else{
+			
+			$emailChecker = checkEmail($email);
+			if ($emailChecker == true) {
+				echo '<div class="alert alert-warning"><strong>Warning:</strong>This Email already exist</div>';
+			}
+			else{
+				$stm="INSERT INTO tbl_student(studentName,fName,mName,email,status)VALUES('$studentName','$fName','$mName','$email','$status')";
 
+				$result = $connection->query($stm);
+				if ($result) {
+					echo '<div class="alert alert-success"><strong>Success : </strong>Data Saved</div>';
+				}
+				else{
+					echo '<div class="alert alert-danger"><strong>Error : </strong>Data Not Saved</div>';
+				}
+			}
+			
+		}
+	}
 
+	function show(){
+		global $connection;
+		$stm = "SELECT * FROM tbl_student";
+		$data = $connection->query($stm);
+		return $data;
+	}
 
-    $emailChecker = checkEmail($email);
-    if($emailChecker == true){
-        echo '<div class="alert alert-warning"><strong>This Email already exist</strong></div>';
-    }
+	function checkEmail($email){
+		global $connection;
+		$stm = $connection->query("SELECT email FROM tbl_student WHERE email = '$email'");
+		
+		if ($stm->num_rows > 0) {
+		 	return true;
+		 } 
+		 else{
+		 	return false;
+		 }
+	}
 
-        else{
+	function findData($id){
+		global $connection;
+		$stm = $connection->query("SELECT * FROM tbl_student WHERE student_id = '$id'");
+		
+		return $stm;
+	}
 
-        $stm = "INSERT INTO tbl_student(studentName,fName,mName,email,status)
-         VALUES('$studentName','$fName','$mName','$email','$status')";
-   
+	function updateData($id, $formData){
+		global $connection;
+		$studentName = $formData['studentName'];
+		$fName = $formData['fName'];
+		$mName = $formData['mName'];
+		$email = $formData['email'];
+		$status = $formData['status'];
 
-            $result = $connection->query($stm);
+		$stm = $connection->query("UPDATE tbl_student SET studentName='$studentName', fName = '$fName', mName = '$mName', email= '$email', status='$status' WHERE student_id = '$id'");
+		if ($stm) {
+			header("location: show.php");
+		}
+		else{
+			echo '<div class="alert alert-danger"><strong>Error:</strong>Data Not Updated</div>'; 
+		}
+	}
 
-    if ($result){
-     echo '<div class="alert alert-success"><strong>Success : Data Saved</strong></div>';
-    }
-    else {
-     echo '<div class="alert alert-danger"><strong>Error : Data Not Saved</strong></div>';
-    }
+	function deleteData($id){
+		global $connection;
+		$stm = $connection->query("DELETE FROM tbl_student WHERE student_id = '$id'");
+		if ($stm) {
+			header("location: show.php");
+		}
+	}
 
-   }
-}
-
-
-
-
-
-
-
-
-function show(){
-
-    global $connection;
-    $connection = new mysqli ("localhost","root","","batch05_php");
-    $stm = "SELECT * FROM  tbl_student";
-    $data = $connection->query($stm);
-
-    return $data;
-
-}
-
-function checkEmail($email){
-    global $connection;
-    $connection = new mysqli ("localhost","root","","batch05_php");
-    $stm = $connection->query("SELECT email FROM  tbl_student WHERE email ='$email'");
-    if($stm ->num_rows > 0)
-    {
-    return true;
-   }
-   else{
-    false;
-   }
-}
-
-   function findData($id){
-    global $connection;
-    $connection = new mysqli ("localhost","root","","batch05_php");
-
-    $stm = $connection->query("SELECT * FROM  tbl_student WHERE student_id ='$id'");
-
- return $stm;
-}
-
-function findName($sName){
-    global $connection;
-    $connection = new mysqli ("localhost","root","","batch05_php");
-
-    $stm = $connection->query("SELECT * FROM  tbl_student WHERE studentName ='$sName'");
-
- return $stm;
-}
-
-function updateData($id, $formData){
-    global $connection;
-    $studentName = $formData["studentName"];
-    $fName = $formData['fName'];
-    $mName = $formData['mName'];
-    $email = $formData['email'];
-    $status= $formData['status'];
-
-     // UPDATE tbl_student SET field name = value WHERE id =value 
-
-      /* $stm = $connection-> query 
-      ("UPDATE tbl_student SET studentName='$studentName',
-      fName='$fName',mName='$mName', email='$email' status='$status' WHERE student_id = '$id'"); 
- */
-      $stm = $connection->query
-      ("UPDATE tbl_student SET studentName='$studentName',
-      fName ='$fName',mName ='$mName', email= '$email', status='$status' WHERE student_id = '$id'");
-
-
-    if($stm){
-        header("location: show.php");
-    }
-    else{
-
-        echo '<div class="alert alert-danger"><strong>Error : Data Not Update</strong></div>';
-    }
-
-  
-}
+	function Active($id){
+		global $connection;
+		$stm = $connection->query("UPDATE tbl_student SET status='1' WHERE student_id = '$id'");
+		if ($stm) {
+			header("location: show.php");
+		}
+	}
+	
+	function Inactive($id){
+		global $connection;
+		$stm = $connection->query("UPDATE tbl_student SET status='2' WHERE student_id = '$id'");
+		if ($stm) {
+			header("location: show.php");
+		}
+	}
--- show.php
+++ show.php
@@ -1,21 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD-Operation</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
 <body>
+	
 
-
-    
-    <div class="container">
+<div class="container">
         <div class="row mt-5">
-            <a href="index.php"class=" mb-3 btn btn-info"> Add Data</a>
+           
+		
+
+				<?php 
+					include "functions.php";
+					if (isset($_GET['id'])) {
+						$id =$_GET['id'];
+						deleteData($id);
+					}
+
+					if (isset($_GET['active'])) {
+						$id =$_GET['active'];
+						Active($id);
+					}
+					if (isset($_GET['inactive'])) {
+						$id =$_GET['inactive'];
+						Inactive($id);
+					}
+
+				?>
+
+          <a href="index.php"class=" mb-3 btn btn-info"> Add Data</a>
             <table class="table" border ="1">
                 <thead>
                     <tr>
@@ -28,31 +47,59 @@
                         <th>Action</th>
                     </tr>
                 </thead>
-                
-              <?php
-              include "functions.php";
-              $data = show();
-             while( $array = $data-> fetch_assoc()){
+					<?php 
+						
+						$data = show();
+						$sl=1;
 
-                echo "<tr>
-                <td>".$array["student_id"]."</td>
-                <td>".$array["studentName"]."</td>
-                <td>".$array["fName"]."</td>
-                <td>".$array["mName"]."</td>
-                <td>".$array["email"]."</td>
-                <td>".$array["status"]."</td>
+						while ($array = $data->fetch_assoc()) {
+							if ($array["status"]==1) {
+								$status ="<a href='show.php?inactive=".$array["student_id"]."' class='btn btn-success btn-sm'>Active</a>";	
+							}
+							else{
+								$status ="<a href='show.php?active=".$array["student_id"]."' class='btn btn-warning btn-sm'>Inactive</a>";
+							}
+							echo "<tr>
+								<td>".$sl."</td>
+								<td>".$array["studentName"]."</td>
+								<td>".$array["fName"]."</td>
+								<td>".$array["mName"]."</td>
+								<td>".$array["email"]."</td>
+								<td>".$status."</td>
+								
+								<td><a href='edit.php?id=".$array["student_id"]."' class='btn btn-info btn-sm'><i class='fa fa-edit'></i></a></td>
+								<td><button class='btn-sm btn btn-danger' data-bs-toggle='modal' data-bs-target='#delete".$array['student_id']."'><i class='fa fa-trash'></i></button></td>
+							</tr>"; ?>
 
-                <td><a href='edit.php?id=".$array["student_id"]."' class='btn btn-info btn-sm'><i class='fa-sharp fa-solid fa-pen-to-square'></i></a></td>
-                <td><a href='edit.php?id=".$array["student_id"]."' class='btn btn-danger btn-sm'><i class='fa-sharp fa-solid fa-trash'></i></a></td>
-                </tr>";  
-             }
-              ?>
 
-            </table>
-            </div>
-        </div>
+							<!-- Modal -->
+<div class="modal fade" id="delete<?php echo $array["student_id"]; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirmation Message</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are you sure want to delete this Data ?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <a href="show.php?id=<?php echo $array["student_id"]; ?>" type="button" class="btn btn-primary">Confirm</a>
+      </div>
     </div>
+  </div>
+</div>
+					<?php $sl++;	}
+					?>
+				</table>	
+			
+		</div>
+	</div>
 
 
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"></script>
 </body>
-</html
+</html>	
